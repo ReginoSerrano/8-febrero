06f7e8effce91341af4d47cbfb4f431ae232aaff
ReginoSerrano <42398512+ReginoSerrano@users.noreply.github.com>
2019-02-17 18:27:48 -0600

    Create dado.js

---
+++ dado.js
@@ -0,0 +1,18 @@
+var x;
+var y;
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255)
+  for(x=5;x<=500;x++)
+  {
+  	for(y=5;y<=500;y++)
+  		{
+   			 ellipse(x,y,10,10);
+    			y=y+10;
+  		}
+  x=x+10;
+  }
+}
